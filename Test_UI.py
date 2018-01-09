1c36ef106bb056451d3f6db56c677e4748abfd9e
San <ltrongsan@yahoo.com>
2018-01-09 03:39:53 +0100

    Update

--- Test_UI.py
+++ Test_UI.py
@@ -12,7 +12,7 @@ class TestUI(Frame):
         self.subtitle_text = StringVar()
         self.subtitle_text.set("MAIN PROGRAM")
         self.subtitle = Label(self.master, textvariable=self.subtitle_text)
-        self.subtitle.grid(row=0, column=1)
+        self.subtitle.grid(row=0, sticky=N)
 
         # self.IP_title_text = StringVar()
         # self.IP_title_text.set("IP")
@@ -24,34 +24,33 @@ class TestUI(Frame):
         # self.Port_title = Label(self.master, textvariable=self.Port_title_text)
         # self.Port_title.grid(row=2, column=1)
 
-        IP_list = ['192.168.0.1', '192.168.1.2']
-        Port_list = ['6686', '6996']
+        client_list = {1: ['192.168.0.1', '6686'],
+                       2: ['192.168.1.2', '6996'],
+                       3: ['192.172.0.3', '6768']}
 
         client_tree = Treeview(self.master)
         client_tree['columns'] = ('ip', 'port', 'status')
-        client_tree.heading("#0", text='No.', anchor='w')
-        client_tree.column("#0", anchor="w")
+        client_tree.heading("#0", text='No.')
+        client_tree.column("#0", anchor='center', width=30)
         client_tree.heading('ip', text='IP address')
-        client_tree.column('ip', anchor='center', width=100)
-        client_tree.heading('port', text='Port number')
-        client_tree.column('port', anchor='center', width=100)
+        client_tree.column('ip', anchor='center', width=200)
+        client_tree.heading('port', text='Port')
+        client_tree.column('port', anchor='center', width=70)
         client_tree.heading('status', text='Status')
         client_tree.column('status', anchor='center', width=100)
+
+        for client_address in client_list.keys():
+            client_tree.insert("", "end", text=client_address, values=(client_list[client_address]))
         client_tree.grid(row=1)
 
         self.record_button = Button(master, text="RECORD", command=self.greet)
-        self.record_button.grid(row=3, column=4)
+        self.record_button.grid(row=1, column=5)
 
-        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button = Button(master, text="CLOSE", command=self.master.quit)
         self.close_button.grid(row=3, column=5)
 
     def greet(self):
         print("Greetings!")
-
-    def cycle_label_text(self, event):
-        self.label_index += 1
-        self.label_index %= len(self.LABEL_TEXT)  # wrap around
-        self.label_text.set(self.LABEL_TEXT[self.label_index])
 
 
 root = Tk()
--- server_part/server_main.py
+++ server_part/server_main.py
@@ -1,15 +1,16 @@
 import socket
-import time
+import sys
 from threading import Thread
 from tkinter import *
+from tkinter.ttk import *
 from server_part import server
+
+root = Tk()
 
 
 def start_server():
-    threads = []
-    threads_id = 1
     while 1:
-        global connection
+        global connection, threads, threads_id
         # now keep talking with the client
         # wait to accept a connection - blocking call
         connection, address = server1.socket.accept()
@@ -29,17 +30,60 @@ port = 8888
 buffer_size = 1024
 sampling_freq = 44100
 
+threads = []
+threads_id = 1
+
 server1 = server.MyServer(host, port)
-Thread(target=start_server).start()
+thread_start_server = Thread(target=start_server).start()
 
-# create a Record Button
-root = Tk()
-root.title('Main Program')
-frame = Frame(root)
-frame.pack()
-record_button = Button(frame, text="RECORD",
-                       bg="red", fg="black",
-                       command=lambda: server1.send_record_command(connection))
-record_button.pack(side=RIGHT)
 
-root.mainloop()
+class MainUI:
+    def __init__(self, master):
+        master.title("MAIN PROGRAM")
+
+        self.subtitle_text = StringVar()
+        self.subtitle_text.set("MAIN PROGRAM")
+        self.subtitle = Label(master, textvariable=self.subtitle_text)
+        self.subtitle.grid(row=0, sticky=N)
+
+        client_list = {1: ['192.168.0.1', '6686'],
+                       2: ['192.168.1.2', '6996'],
+                       3: ['192.172.0.3', '6768']}
+
+        client_tree = Treeview(master)
+        client_tree['columns'] = ('ip', 'port', 'status')
+        client_tree.heading("#0", text='No.')
+        client_tree.column("#0", anchor='center', width=30)
+        client_tree.heading('ip', text='IP address')
+        client_tree.column('ip', anchor='center', width=200)
+        client_tree.heading('port', text='Port')
+        client_tree.column('port', anchor='center', width=70)
+        client_tree.heading('status', text='Status')
+        client_tree.column('status', anchor='center', width=100)
+
+        for client_address in client_list.keys():
+            client_tree.insert("", "end", text=client_address, values=(client_list[client_address]))
+        client_tree.grid(row=1)
+
+        self.record_button = Button(master, text="RECORD", command=server1.send_record_command)
+        self.record_button.grid(row=1, column=5)
+
+        self.close_button = Button(master, text="CLOSE", command=onExit)
+        self.close_button.grid(row=3, column=5)
+
+
+def main():
+    MainUI(root)
+    root.mainloop()
+
+
+def onExit():
+    thread_start_server.join()
+    for thread in threads:
+        thread.shutdown_flag.set()
+        thread.join()
+    root.quit()
+
+
+
+main()
